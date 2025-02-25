d2a3050214d260daa745389ba1a8b4a2b1e97d4f
syed-shaida-hussain <85978159+syed-shaida-hussain@users.noreply.github.com>
2025-02-25 14:04:01 +0530

    Update ThemeSlice.js

--- src/app/GlobalRedux/features/theme/ThemeSlice.js
+++ src/app/GlobalRedux/features/theme/ThemeSlice.js
@@ -2,7 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 const isServer = typeof window === "undefined";
 
 const initialState = {
-    theme : !isServer && (localStorage.getItem("theme") ?? "light")
+    theme : localStorage.getItem("theme") ?? "light"
 }
 if(!isServer) {
 }
